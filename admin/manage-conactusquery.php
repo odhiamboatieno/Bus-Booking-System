007213adb81cc2fc053fb553a8e03291242cd711
Odhiambo Atieno <odhiamboisatieno@gmail.com>
2021-05-28 11:18:31 -0400

    Update manage-conactusquery.php

--- admin/manage-conactusquery.php
+++ admin/manage-conactusquery.php
@@ -35,7 +35,7 @@ $msg="Testimonial Successfully Inacrive";
 	<meta name="author" content="">
 	<meta name="theme-color" content="#3e454c">
 	
-	<title>Limo Transport Sacco |Admin Manage Queries   </title>
+	<title>Odhiambo Atieno Transport Sacco |Admin Manage Queries   </title>
 
 	<!-- Font awesome -->
 	<link rel="stylesheet" href="css/font-awesome.min.css">
